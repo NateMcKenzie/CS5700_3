bea9cb6fa3fe05c8a75b5d634f9e4488a9f126c4
Nate McKenzie <nate6264@gmail.com>
2024-07-08 11:06:15 -0600

    Add ShippingUpdate

--- src/main/kotlin/ShippingUpdate.kt
+++ src/main/kotlin/ShippingUpdate.kt
@@ -1,2 +1,11 @@
-class ShippingUpdate {
+class ShippingUpdate (
+    shipment: Shipment,
+    val timestamp: Long,
+    val newStatus: String = shipment.status,
+    val newDeliveryDate: Long = shipment.expectedDeliveryDateTimestamp,
+    val newLocation: String = shipment.currentLocation
+){
+    val previousStatus = shipment.status
+    val previousDeliveryDate = shipment.expectedDeliveryDateTimestamp
+    val previousLocation = shipment.currentLocation
 }
